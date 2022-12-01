7a5a9817df358d846e793a336cbf7714246a0e3e
Ulyana <ulyana.khanbekova@gmail.com>
2022-12-01 17:39:25 +0300

    Add 1st day

---
+++ 1_1.py
@@ -0,0 +1,17 @@
+max_num = 0
+buffer = 0
+
+with open("input.txt", 'r') as file:
+    for line in file:
+        line = line.rstrip('\n')
+        #print(f"line={line}, buffer={buffer}, max_num={max_num}")
+        if line != "":
+            buffer += int(line)
+        else:
+            max_num = max(max_num, buffer)
+            buffer = 0
+
+print(max_num)
+
+        
+    
