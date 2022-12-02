6708ced76b4f185cdeecfe4e2defaa595ac50ab1
Ulyana <ulyana.khanbekova@gmail.com>
2022-12-02 14:50:08 +0300

    Add 2_1

---
+++ 2/2_1.py
@@ -0,0 +1,17 @@
+rules = {
+    0 : ["BX", "CY", "AZ"],
+    3 : ["AX", "BY", "CZ"],
+    6 : ["CX", "AY", "BZ"]
+    }
+
+score = 0
+
+with open("input.txt", "r") as file:
+    rounds = [line.replace(" ", '') for line in file.read().split("\n")]
+    
+    for key, value in rules.items():
+        for i, v in enumerate(value):
+            score += rounds.count(v) * (key+ i +1)
+
+    print(score)
+    
